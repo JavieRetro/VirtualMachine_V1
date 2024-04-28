487d9140ad4261a64f0ffd91c5d69aff8e3ffbd2
Javier Olivas <javier.olivas.blanco@colegiolitterator.com>
2024-04-28 20:39:13 +0200

    Clase resuelta a la espera de las otras clases

--- Maquina_Virtual/src/Main/Memory.java
+++ Maquina_Virtual/src/Main/Memory.java
@@ -7,43 +7,69 @@ public class Memory {
 	private int max_Memory;
 	
 	//Constructora
-		public Memory() {
-			this.Memory = new Integer[size];
-			this.empty = true;
-		}
-		
-		private void resize(int pos) {
-			if(pos >= size) {
-				this.empty = false;
-				Integer[]Memory2 = new Integer[pos * 2];
-				
-				for(int i = 0; i < this.max_Memory; i++) {
-					
-					Memory2[i] = this.Memory[i];
-				}
-				
-				this.Memory = Memory2;
-			}
-		}
-		
-		public boolean write(int posicion, int valor) {
-			if(posicion >= 0) {
-				this.resize(posicion);
-				this.Memory[posicion] = valor;
-				return true;
-			}else {
-				return false;
-			}
-		}
-		
-		public Integer read (int posicion) {
-			if(this.Memory[posicion] == null) {
-				
-			return -1;
-			}else {
-				return this.Memory[posicion] = posicion;
-			}
+	public Memory() {
+		this.Memory = new Integer[size];
+		this.empty = true;
+	}
+	
+     
+	@Override
+	//metodo toString
+	public String toString() {
+		String memory = " ";
+		if (isEmpty() == false) {
+		for(int i = 0; i < size; i++) {
 			
-		}
+			memory += this.Memory[size] + " , ";
+				
+			}
+		return memory;
+		}else {
+		return null;
+	}
 }
+	
+	
+	private boolean isEmpty() {
+		// TODO Auto-generated method stub
+		if(size > 0) {
+			return true;
+		}else {
+		return false;
+		}
+	}
 
+	private void resize (int posicion) {
+		if(posicion >= size) {
+			this.empty = false;
+			Integer[] Memory2 = new Integer[posicion*2];
+			for (int i = 0; i < this.Memory.length; i++) {
+	            Memory2[i] = this.Memory[i];
+	        }
+			this.Memory = Memory2;
+		}
+	}
+	
+
+	//metodo write(pos, value) return tipo boolean si se puede escribir, devolver true;
+	public boolean write(int posicion, int valor) {
+		if(posicion >= 0) {
+			this.resize(posicion);
+			this.Memory[posicion] = valor;
+			return true;
+		}else {
+		
+	return false;
+	}
+}	
+	//metodo read(pos) return integer por el array;
+	public Integer read(int posicion) {
+		if(this.Memory[posicion] == null) {
+			return null;
+		}else {
+			return this.Memory[posicion];
+		}
+	}
+	
+
+}
