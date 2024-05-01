e656f09bf01fc769ea218f84143d7f592bc4f74e
Javier Olivas <javier.olivas.blanco@colegiolitterator.com>
2024-05-02 00:38:20 +0200

    Primer avance de la clase,

--- Maquina_Virtual/src/Main/ByteCodeParser.java
+++ Maquina_Virtual/src/Main/ByteCodeParser.java
@@ -2,4 +2,41 @@ package Main;
 
 public class ByteCodeParser {
 
+	public static ByteCode parse(String line) {
+		// TODO Auto-generated method stub
+		String[] cadena = line.split(" ");
+		
+		switch(cadena.length) {
+		
+		case 1:
+			
+			if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("push")) {
+				return new ByteCode(ENUM_BYTECODE.PUSH);
+			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("load")) {
+				return new ByteCode(ENUM_BYTECODE.LOAD);
+			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("store")) {
+				return new ByteCode(ENUM_BYTECODE.STORE);
+			}
+			
+		case 2:
+			if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("add")) {
+				return new ByteCode(ENUM_BYTECODE.ADD);
+			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("sub")) {
+				return new ByteCode(ENUM_BYTECODE.SUB);	
+			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("div")) {
+				return new ByteCode(ENUM_BYTECODE.DIV);
+			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("mul")) {
+				return new ByteCode(ENUM_BYTECODE.MUL);
+			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("out")) {
+				return new ByteCode(ENUM_BYTECODE.OUT);
+			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("halt")) {
+				return new ByteCode(ENUM_BYTECODE.HALT);
+			}
+			default:
+			
+				return null;
+		}
+		
+	}
+
 }
