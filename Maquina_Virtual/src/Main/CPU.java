665d9415974cfa84aba366929cd5d4b0af3830bb
Javier Olivas <javier.olivas.blanco@colegiolitterator.com>
2024-05-02 00:39:34 +0200

    Primeros métodos

--- Maquina_Virtual/src/Main/CPU.java
+++ Maquina_Virtual/src/Main/CPU.java
@@ -1,5 +1,48 @@
 package Main;
 
 public class CPU {
+	
+	public OperandStack Stack;
+	public Memory memory;
+	public boolean fin;
+	
+	public CPU() {
+		
+	}
+	
+	public void erase() {
+		
+	}
+	
+	public boolean execute(ByteCode instr) {
+		
+		if(instr.equals(ENUM_BYTECODE.ADD)) {
+			sumaPila();
+			return true;
+		}
+		return false;
+	}
+
+	public void sumaPila() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public void restaPila() {
+		
+	}
+	
+	public void multiplicarPila() {
+		
+	}
+	
+	public void dividirPila() {
+		
+	}
+	
+	public boolean isHalt() {
+		return fin;
+	}
+	
 
 }
