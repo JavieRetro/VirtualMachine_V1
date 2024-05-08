00b50e4decf9ba0963e9e596bdda7efd484b8c4b
Javier Olivas <javier.olivas.blanco@colegiolitterator.com>
2024-05-08 16:36:22 +0200

    Clase finalizada

--- Maquina_Virtual/src/Main/ByteCode.java
+++ Maquina_Virtual/src/Main/ByteCode.java
@@ -2,4 +2,23 @@ package Main;
 
 public class ByteCode {
 
+	private ENUM_BYTECODE name;
+	private int valor;
+	
+	public ByteCode(ENUM_BYTECODE name) {
+		this.name = name;
+	}
+	
+	public ByteCode(ENUM_BYTECODE name, Integer valor){
+		this.name = name;
+		this.valor = valor;
+	}
+
+    public ENUM_BYTECODE getByteCode() {
+    	return this.name;
+    }
+    
+    public int getValor() {
+    	return this.valor;
+    }
 }
