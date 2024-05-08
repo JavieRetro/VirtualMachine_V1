6153e90bf447b895ce9887603bd7cbd5a0a69457
Javier Olivas <javier.olivas.blanco@colegiolitterator.com>
2024-05-08 17:32:41 +0200

    Clase corregida y finalizada

--- Maquina_Virtual/src/Main/ByteCodeParser.java
+++ Maquina_Virtual/src/Main/ByteCodeParser.java
@@ -4,36 +4,45 @@ public class ByteCodeParser {
 
 	public static ByteCode parse(String s) {
 		// TODO Auto-generated method stub
-		String[] cadenaBc = s.split(" ");
+		String[] arrayBc = s.split(" ");
 		
 		if(s.length() == 0 || s.length() > 2) {
 			return null;
 			
 		} else if(s.length() == 1) {
 			
-			switch(cadenaBc[1]) {
+			switch(arrayBc[1]) {
 			        case "ADD":
+			        	return new ByteCode(ENUM_BYTECODE.ADD);
 			        
 			        case "SUB":
+			        	return new ByteCode(ENUM_BYTECODE.SUB);
 			        	
 			        case "DIV":
+			        	return new ByteCode(ENUM_BYTECODE.DIV);
 			        
 			        case "MUL":
+			        	return new ByteCode(ENUM_BYTECODE.MUL);
 			        	
 			        case "OUT":
+			        	return new ByteCode(ENUM_BYTECODE.OUT);
 			        	
 			        case "HALT":
+			        	return new ByteCode(ENUM_BYTECODE.HALT);
 			        	
 			        default:
 			        	return null;		
 			}
 		}else {
-			switch(cadenaBc[1]) {
+			switch(arrayBc[0]) {
 			case "PUSH":
+				return new ByteCode(ENUM_BYTECODE.PUSH, Integer.parseInt(arrayBc[1]));
 		        
 	        case "LOAD":
+	        	return new ByteCode(ENUM_BYTECODE.LOAD, Integer.parseInt(arrayBc[1]));
 	        	
 	        case "STORE":
+	        	return new ByteCode(ENUM_BYTECODE.STORE, Integer.parseInt(arrayBc[1]));
 			
 	        default:
 	        	return null;
