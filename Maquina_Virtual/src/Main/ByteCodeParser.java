c1bae3b28839ad1ee5178e33b14a81930ff06e92
Javier Olivas <javier.olivas.blanco@colegiolitterator.com>
2024-05-08 16:30:54 +0200

    pendiente de correción

--- Maquina_Virtual/src/Main/ByteCodeParser.java
+++ Maquina_Virtual/src/Main/ByteCodeParser.java
@@ -2,39 +2,42 @@ package Main;
 
 public class ByteCodeParser {
 
-	public static ByteCode parse(String line) {
+	public static ByteCode parse(String s) {
 		// TODO Auto-generated method stub
-		String[] cadena = line.split(" ");
+		String[] cadenaBc = s.split(" ");
 		
-		switch(cadena.length) {
-		
-		case 1:
+		if(s.length() == 0 || s.length() > 2) {
+			return null;
 			
-			if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("push")) {
-				return new ByteCode(ENUM_BYTECODE.PUSH);
-			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("load")) {
-				return new ByteCode(ENUM_BYTECODE.LOAD);
-			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("store")) {
-				return new ByteCode(ENUM_BYTECODE.STORE);
+		} else if(s.length() == 1) {
+			
+			switch(cadenaBc[1]) {
+			        case "ADD":
+			        
+			        case "SUB":
+			        	
+			        case "DIV":
+			        
+			        case "MUL":
+			        	
+			        case "OUT":
+			        	
+			        case "HALT":
+			        	
+			        default:
+			        	return null;		
 			}
+		}else {
+			switch(cadenaBc[1]) {
+			case "PUSH":
+		        
+	        case "LOAD":
+	        	
+	        case "STORE":
 			
-		case 2:
-			if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("add")) {
-				return new ByteCode(ENUM_BYTECODE.ADD);
-			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("sub")) {
-				return new ByteCode(ENUM_BYTECODE.SUB);	
-			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("div")) {
-				return new ByteCode(ENUM_BYTECODE.DIV);
-			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("mul")) {
-				return new ByteCode(ENUM_BYTECODE.MUL);
-			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("out")) {
-				return new ByteCode(ENUM_BYTECODE.OUT);
-			}else if(cadena[0].equalsIgnoreCase("newinst") && cadena[1].equalsIgnoreCase("halt")) {
-				return new ByteCode(ENUM_BYTECODE.HALT);
+	        default:
+	        	return null;
 			}
-			default:
-			
-				return null;
 		}
 		
 	}
