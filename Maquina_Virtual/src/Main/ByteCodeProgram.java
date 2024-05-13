44198be348df0b7f8467fe393e6e73b7ae793709
Javier Olivas <javier.olivas.blanco@colegiolitterator.com>
2024-05-14 01:00:53 +0200

    Clase con error en el resize

--- Maquina_Virtual/src/Main/ByteCodeProgram.java
+++ Maquina_Virtual/src/Main/ByteCodeProgram.java
@@ -4,41 +4,54 @@ import java.util.Iterator;
 
 public class ByteCodeProgram {
 	private int num_Elems;
-	private final int size = 10;
+	private final int size;
 	private ByteCode[]programa;
 	
 	
 	public ByteCodeProgram() {
+		this.size = 10;
 		this.num_Elems = 0;
-		this.programa = new ByteCode[size];
+		this.programa = new ByteCode[this.size];
 	}
 	
 	
 	public void resize() {
-		if(this.num_Elems >= this.size) {
-			Bytecode[]bc = new Bytecode[size * 2];
-			
-			for(int i = 0; i < this.programa.length; i++) {
-				bc[i] = this.programa[i];
-			}
-			this.programa = bc;
+		Bytecode programa2 = new Bytecode[this.size * 2];
+		for(int i = 0; i < this.programa.length; i++) {
+			programa2[i] = this.programa[i];
 		}
+		this.programa = programa2;
 	}
 	
 	public void setInstruction(ByteCode bc) {
-		
-		this.resize();
-		this.programa[this.num_Elems] = bc;
-		this.num_Elems++;
+		if(this.num_Elems >= this.programa.length) {
+			this.resize();
+			this.programa[this.num_Elems] = bc;
+			this.num_Elems++;
+		}else {
+			this.programa[this.num_Elems] = bc;
+			this.num_Elems++;
+		}
 	}
 	
-	public String toString() {
-		String cadena = "";
-		for(int i = 0; i < this.programa.length; i++) {
-			cadena += this.programa[i];
-		}
-		return cadena;
+	public ByteCode getInstruction() {
+		return this.programa[this.num_Elems];
+	}
+	
+	public String runProgram(CPU cpu) {
 		
+		String mensaje = " ";
+		for(int i = 0; i < this.num_Elems; i++) { //Ejecutar las instrucciones que tenga el programa
+			if(!cpu.isHalt() && cpu.execute(this.programa[i])) {
+				//texto de la maquina virtual
+				
+			}else if(!cpu.isHalt()){
+				//texto de la maquina virtual
+			}
+		}
+		cpu.erase();
+		cpu.runCPU();
+		return mensaje;
 	}
 
 }
