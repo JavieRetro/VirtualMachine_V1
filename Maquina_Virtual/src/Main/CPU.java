edc47382d61125dab3d9cd7c818744c16cbade9a
Javier Olivas <javier.olivas.blanco@colegiolitterator.com>
2024-05-14 01:06:29 +0200

    Incorporación de nuevas funciones

--- Maquina_Virtual/src/Main/CPU.java
+++ Maquina_Virtual/src/Main/CPU.java
@@ -11,6 +11,8 @@ public class CPU {
 	}
 	
 	public void erase() {
+		this.memory = new Memory();
+		this.Stack = new OperandStack();
 		
 	}
 	
@@ -20,29 +22,88 @@ public class CPU {
 			sumaPila();
 			return true;
 		}
+		else if(instr.equals(ENUM_BYTECODE.SUB)) {
+			restaPila();
+			return true;
+		}
+		else if(instr.equals(ENUM_BYTECODE.MUL)) {
+			multiplicarPila();
+			return true;
+		}
+		else if(instr.equals(ENUM_BYTECODE.DIV)) {
+			dividirPila();
+			return true;	
+		}
+		else if(instr.equals(ENUM_BYTECODE.HALT)) {
+			isHalt();
+			return true;
+		}
+		else if(instr.equals(ENUM_BYTECODE.STORE)) {
+			storePila();
+			return true;
+		}
+		else if(instr.equals(ENUM_BYTECODE.LOAD)) {
+			cerrarPila();
+			return true;
+		}
 		return false;
 	}
 
-	public void sumaPila() {
+	
+
+	public boolean sumaPila() {
 		// TODO Auto-generated method stub
+		int num1 = pila.pop();
+		int num2 = pila.pop();
+		
+		int suma = num1 + num2;
+		pila.push(suma);
+		return true;
 		
 	}
 	
-	public void restaPila() {
+	public boolean restaPila() {
+		int num1 = pila.pop();
+		int num2 = pila.pop();
 		
+		int resta = num1 - num2;
+		pila.push(resta);
+		return true;
 	}
 	
-	public void multiplicarPila() {
+	public boolean multiplicarPila() {
+		int num1 = pila.pop();
+		int num2 = pila.pop();
 		
+		int multiplicación = num1 * num2;
+		pila.push(multiplicación);
+		return true;
 	}
 	
-	public void dividirPila() {
+	public boolean dividirPila() {
+		int num1 = pila.pop();
+		int num2 = pila.pop();
+		
+		int division = num1 / num2;
+		pila.push(division);
+		return true;
 		
 	}
 	
 	public boolean isHalt() {
-		return fin;
+		if(!this.isHalt()) {
+			return true;
+		}else {
+			return false;
+		}
 	}
-	
+	private void storePila() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void cerrarPila() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
