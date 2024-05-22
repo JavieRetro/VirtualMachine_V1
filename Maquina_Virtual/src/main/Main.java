b7f1ef03f288c512c2456e0861ef4a5963843e74
Javier Olivas <javier.olivas.blanco@colegiolitterator.com>
2024-05-22 13:17:17 +0200

    comentarios incluidos

--- Maquina_Virtual/src/main/Main.java
+++ Maquina_Virtual/src/main/Main.java
@@ -6,7 +6,7 @@ public class Main {
      // TODO Auto-generated method stub
 	/**
 	 * 
-	 * @param engine
+	 * @param args
 	 */
 	public static void main(String[] args) {
 		Engine eng = new Engine();
